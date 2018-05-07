ab28854be118e0340d4ab23fa66b764e133722ff
valentinancy <nensivalentina@gmail.com>
2018-05-07 09:22:35 +0700

    new textures

--- js/Main.js
+++ js/Main.js
@@ -349,3 +349,15 @@ function thumbnailClicked(id, src) {
 
     changeMaterial(id, src);
 }
+
+window.addEventListener("error", handleError, true);
+
+function handleError(evt) {
+    if (evt.message) { // Chrome sometimes provides this
+      alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
+      console.log("error: ",evt.message ," at linenumber: ",evt.lineno," of file: ",evt.filename);
+    } else {
+      alert("TERDAPAT KESALAHAN PADA BERKAS JSON YANG ANDA UNGGAH PADA BAGIAN "+(evt.srcElement || evt.target));
+      console.log("error: ",evt.type," from element: ",(evt.srcElement || evt.target));
+    }
+}
